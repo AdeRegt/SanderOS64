9695a393a77b60f8f875dcd2e1d5cfd2f35e936e
AdeRegt <alexandrosderegtvisvliet@gmail.com>
2022-09-18 23:28:35 +0200

    Adding cdrom support

---
+++ bootloader/GRUB32/boot.asm
@@ -0,0 +1,225 @@
+[bits 32]
+
+; Declare constants for the multiboot header.
+MBALIGN  equ  1 << 0            ; align loaded modules on page boundaries
+MEMINFO  equ  1 << 1            ; provide memory map
+FLAGS    equ  MBALIGN | MEMINFO ; this is the Multiboot 'flag' field
+MAGIC    equ  0x1BADB002        ; 'magic number' lets bootloader find the header
+CHECKSUM equ -(MAGIC + FLAGS)   ; checksum of above, to prove we are multiboot
+ 
+; Declare a multiboot header that marks the program as a kernel. These are magic
+; values that are documented in the multiboot standard. The bootloader will
+; search for this signature in the first 8 KiB of the kernel file, aligned at a
+; 32-bit boundary. The signature is in its own section so the header can be
+; forced to be within the first 8 KiB of the kernel file.
+section .multiboot
+align 4
+	dd MAGIC
+	dd FLAGS
+	dd CHECKSUM
+ 
+; The multiboot standard does not define the value of the stack pointer register
+; (esp) and it is up to the kernel to provide a stack. This allocates room for a
+; small stack by creating a symbol at the bottom of it, then allocating 16384
+; bytes for it, and finally creating a symbol at the top. The stack grows
+; downwards on x86. The stack is in its own section so it can be marked nobits,
+; which means the kernel file is smaller because it does not contain an
+; uninitialized stack. The stack on x86 must be 16-byte aligned according to the
+; System V ABI standard and de-facto extensions. The compiler will assume the
+; stack is properly aligned and failure to align the stack will result in
+; undefined behavior.
+section .bss
+align 16
+stack_bottom:
+resb 16384 ; 16 KiB
+stack_top:
+ 
+; The linker script specifies _start as the entry point to the kernel and the
+; bootloader will jump to this position once the kernel has been loaded. It
+; doesn't make sense to return from this function as the bootloader is gone.
+; Declare _start as a function symbol with the given symbol size.
+section .text
+global _start:function (end - _start)
+_start:
+	; The bootloader has loaded us into 32-bit protected mode on a x86
+	; machine. Interrupts are disabled. Paging is disabled. The processor
+	; state is as defined in the multiboot standard. The kernel has full
+	; control of the CPU. The kernel can only make use of hardware features
+	; and any code it provides as part of itself. There's no printf
+	; function, unless the kernel provides its own <stdio.h> header and a
+	; printf implementation. There are no security restrictions, no
+	; safeguards, no debugging mechanisms, only what the kernel provides
+	; itself. It has absolute and complete power over the
+	; machine.
+ 
+	; To set up a stack, we set the esp register to point to the top of our
+	; stack (as it grows downwards on x86 systems). This is necessarily done
+	; in assembly as languages such as C cannot function without a stack.
+	mov esp, stack_top
+ 
+	; This is a good place to initialize crucial processor state before the
+	; high-level kernel is entered. It's best to minimize the early
+	; environment where crucial features are offline. Note that the
+	; processor is not fully initialized yet: Features such as floating
+	; point instructions and instruction set extensions are not initialized
+	; yet. The GDT should be loaded here. Paging should be enabled here.
+	; C++ features such as global constructors and exceptions will require
+	; runtime support to work as well.
+ 
+	; Enter the high-level kernel. The ABI requires the stack is 16-byte
+	; aligned at the time of the call instruction (which afterwards pushes
+	; the return pointer of size 4 bytes). The stack was originally 16-byte
+	; aligned above and we've since pushed a multiple of 16 bytes to the
+	; stack since (pushed 0 bytes so far) and the alignment is thus
+	; preserved and the call is well defined.
+        ; note, that if you are building on Windows, C functions may have "_" prefix in assembly: _kernel_main
+    push eax ; header magic of grub
+	push ebx ; header pointer
+	extern kernel_main
+	call kernel_main
+ 
+	; If the system has nothing more to do, put the computer into an
+	; infinite loop. To do that:
+	; 1) Disable interrupts with cli (clear interrupt enable in eflags).
+	;    They are already disabled by the bootloader, so this is not needed.
+	;    Mind that you might later enable interrupts and return from
+	;    kernel_main (which is sort of nonsensical to do).
+	; 2) Wait for the next interrupt to arrive with hlt (halt instruction).
+	;    Since they are disabled, this will lock up the computer.
+	; 3) Jump to the hlt instruction if it ever wakes up due to a
+	;    non-maskable interrupt occurring or due to system management mode.
+
+    ;
+    ; inspiration:
+    ; https://wiki.osdev.org/Setting_Up_Long_Mode
+    ;
+
+    ; check if 64bits is possible...
+    mov eax, 0x80000000
+    cpuid
+    cmp eax, 0x80000001
+    jb NoLongMode
+    mov eax, 0x80000001
+    cpuid
+    test edx, 1 << 29
+    jz NoLongMode
+
+    mov eax, cr0                                   ; Set the A-register to control register 0.
+    and eax, 01111111111111111111111111111111b     ; Clear the PG-bit, which is bit 31.
+    mov cr0, eax                                   ; Set control register 0 to the A-register.
+
+    mov edi, 0x1000    ; Set the destination index to 0x1000.
+    mov cr3, edi       ; Set control register 3 to the destination index.
+    xor eax, eax       ; Nullify the A-register.
+    mov ecx, 4096      ; Set the C-register to 4096.
+    rep stosd          ; Clear the memory.
+    mov edi, cr3       ; Set the destination index to control register 3.
+
+    mov DWORD [edi], 0x2003      ; Set the uint32_t at the destination index to 0x2003.
+    add edi, 0x1000              ; Add 0x1000 to the destination index.
+    mov DWORD [edi], 0x3003      ; Set the uint32_t at the destination index to 0x3003.
+    add edi, 0x1000              ; Add 0x1000 to the destination index.
+    mov DWORD [edi], 0x4003      ; Set the uint32_t at the destination index to 0x4003.
+    add edi, 0x1000              ; Add 0x1000 to the destination index.
+
+    mov ebx, 0x00000003          ; Set the B-register to 0x00000003.
+    mov ecx, 512                 ; Set the C-register to 512.
+ 
+.SetEntry:
+    mov DWORD [edi], ebx         ; Set the uint32_t at the destination index to the B-register.
+    add ebx, 0x1000              ; Add 0x1000 to the B-register.
+    add edi, 8                   ; Add eight to the destination index.
+    loop .SetEntry               ; Set the next entry.
+
+    mov eax, cr4                 ; Set the A-register to control register 4.
+    or eax, 1 << 5               ; Set the PAE-bit, which is the 6th bit (bit 5).
+    mov cr4, eax                 ; Set control register 4 to the A-register.
+
+;     mov eax, 0x7                 ; You might want to check for page 7 first!
+;     xor ecx, ecx
+;     cpuid
+;     test ecx, (1<<16)
+;     jnz .5_level_paging
+
+; [extern nomodemessager]
+
+; .5_level_paging:
+;     mov eax, cr4
+;     or eax, (1<<12) ;CR4.LA57
+;     mov cr4, eax
+
+    mov ecx, 0xC0000080          ; Set the C-register to 0xC0000080, which is the EFER MSR.
+    rdmsr                        ; Read from the model-specific register.
+    or eax, 1 << 8               ; Set the LM-bit which is the 9th bit (bit 8).
+    wrmsr                        ; Write to the model-specific register.
+
+    mov eax, cr0                 ; Set the A-register to control register 0.
+    or eax, 1 << 31              ; Set the PG-bit, which is the 32nd bit (bit 31).
+    mov cr0, eax                 ; Set control register 0 to the A-register.
+
+    lgdt [GDT.Pointer]         ; Load the 64-bit global descriptor table.
+    jmp GDT.Code:Realm64       ; Set the code segment and enter 64-bit long mode.
+
+    jmp eol
+
+[extern nolongmodemessager]
+NoLongMode:
+    call nolongmodemessager
+eol:
+	cli
+hang:	
+    hlt
+	jmp hang
+end:
+
+; Access bits
+PRESENT        equ 1 << 7
+NOT_SYS        equ 1 << 4
+EXEC           equ 1 << 3
+DC             equ 1 << 2
+RW             equ 1 << 1
+ACCESSED       equ 1 << 0
+ 
+; Flags bits
+GRAN_4K       equ 1 << 7
+SZ_32         equ 1 << 6
+LONG_MODE     equ 1 << 5
+ 
+GDT:
+    .Null: equ $ - GDT
+        dq 0
+    .Code: equ $ - GDT
+        dd 0xFFFF                                   ; Limit & Base (low, bits 0-15)
+        db 0                                        ; Base (mid, bits 16-23)
+        db PRESENT | NOT_SYS | EXEC | RW            ; Access
+        db GRAN_4K | LONG_MODE | 0xF                ; Flags & Limit (high, bits 16-19)
+        db 0                                        ; Base (high, bits 24-31)
+    .Data: equ $ - GDT
+        dd 0xFFFF                                   ; Limit & Base (low, bits 0-15)
+        db 0                                        ; Base (mid, bits 16-23)
+        db PRESENT | NOT_SYS | RW                   ; Access
+        db GRAN_4K | SZ_32 | 0xF                    ; Flags & Limit (high, bits 16-19)
+        db 0                                        ; Base (high, bits 24-31)
+    .TSS: equ $ - GDT
+        dd 0x00000068
+        dd 0x00CF8900
+    .Pointer:
+        dw $ - GDT - 1
+        dq GDT
+
+; Use 64-bit.
+[BITS 64]
+
+extern epoint
+
+Realm64:
+    cli                           ; Clear the interrupt flag.
+    mov ax, GDT.Data            ; Set the A-register to the data descriptor.
+    mov ds, ax                    ; Set the data segment to the A-register.
+    mov es, ax                    ; Set the extra segment to the A-register.
+    mov fs, ax                    ; Set the F-segment to the A-register.
+    mov gs, ax                    ; Set the G-segment to the A-register.
+    mov ss, ax                    ; Set the stack segment to the A-register.
+    
+    
+    call qword [epoint]
---
+++ bootloader/GRUB32/kernel.c
@@ -0,0 +1,374 @@
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+
+/* Type for a 16-bit quantity.  */
+typedef uint16_t Elf32_Half;
+typedef uint16_t Elf64_Half;
+
+/* Types for signed and unsigned 32-bit quantities.  */
+typedef uint32_t Elf32_Word;
+typedef	int32_t  Elf32_Sword;
+typedef uint32_t Elf64_Word;
+typedef	int32_t  Elf64_Sword;
+
+/* Types for signed and unsigned 64-bit quantities.  */
+typedef uint64_t Elf32_Xword;
+typedef	int64_t  Elf32_Sxword;
+typedef uint64_t Elf64_Xword;
+typedef	int64_t  Elf64_Sxword;
+
+/* Type of addresses.  */
+typedef uint32_t Elf32_Addr;
+typedef uint64_t Elf64_Addr;
+
+/* Type of file offsets.  */
+typedef uint32_t Elf32_Off;
+typedef uint64_t Elf64_Off;
+
+/* Type for section indices, which are 16-bit quantities.  */
+typedef uint16_t Elf32_Section;
+typedef uint16_t Elf64_Section;
+
+/* Type for version symbol information.  */
+typedef Elf32_Half Elf32_Versym;
+typedef Elf64_Half Elf64_Versym;
+
+typedef struct
+{
+  Elf64_Word	p_type;			/* Segment type */
+  Elf64_Word	p_flags;		/* Segment flags */
+  Elf64_Off	p_offset;		/* Segment file offset */
+  Elf64_Addr	p_vaddr;		/* Segment virtual address */
+  Elf64_Addr	p_paddr;		/* Segment physical address */
+  Elf64_Xword	p_filesz;		/* Segment size in file */
+  Elf64_Xword	p_memsz;		/* Segment size in memory */
+  Elf64_Xword	p_align;		/* Segment alignment */
+} Elf64_Phdr;
+
+#define EI_NIDENT (16)
+typedef struct
+{
+  unsigned char	e_ident[EI_NIDENT];	/* Magic number and other info */
+  Elf64_Half	e_type;			/* Object file type */
+  Elf64_Half	e_machine;		/* Architecture */
+  Elf64_Word	e_version;		/* Object file version */
+  Elf64_Addr	e_entry;		/* Entry point virtual address */
+  Elf64_Off	e_phoff;		/* Program header table file offset */
+  Elf64_Off	e_shoff;		/* Section header table file offset */
+  Elf64_Word	e_flags;		/* Processor-specific flags */
+  Elf64_Half	e_ehsize;		/* ELF header size in bytes */
+  Elf64_Half	e_phentsize;		/* Program header table entry size */
+  Elf64_Half	e_phnum;		/* Program header table entry count */
+  Elf64_Half	e_shentsize;		/* Section header table entry size */
+  Elf64_Half	e_shnum;		/* Section header table entry count */
+  Elf64_Half	e_shstrndx;		/* Section header string table index */
+} Elf64_Ehdr;
+
+typedef struct
+{
+  Elf64_Word sh_name; /* Section name */
+  Elf64_Word sh_type; /* Section type */
+  Elf64_Word sh_flagsA; /* Section attributes */
+  Elf64_Word sh_flagsB; /* Section attributes */
+  Elf32_Addr sh_addrA; /* Virtual address in memory */
+  Elf32_Addr sh_addrB; /* Virtual address in memory */
+  Elf32_Off sh_offsetA; /* Offset in file */
+  Elf32_Off sh_offsetB; /* Offset in file */
+  Elf64_Word sh_sizeA; /* Size of section */
+  Elf64_Word sh_sizeB; /* Size of section */
+  Elf64_Word sh_link; /* Link to other section */
+  Elf64_Word sh_info; /* Miscellaneous information */
+  Elf64_Xword sh_addralign; /* Address alignment boundary */
+  Elf64_Xword sh_entsize; /* Size of entries, if section has table */
+} Elf64_Shdr;
+
+typedef struct
+{
+  Elf64_Addr r_offset; /* Address of reference */
+  Elf64_Xword r_info; /* Symbol index and type of relocation */
+} Elf64_Rel;
+
+typedef struct
+{
+  Elf64_Addr r_offset; /* Address of reference */
+  Elf64_Xword r_info; /* Symbol index and type of relocation */
+  Elf64_Sxword r_addend; /* Constant part of expression */
+} Elf64_Rela;
+
+typedef struct
+{
+  Elf64_Word st_name; /* Symbol name */
+  unsigned char st_info; /* Type and Binding attributes */
+  unsigned char st_other; /* Reserved */
+  Elf64_Half st_shndx; /* Section table index */
+  Elf64_Addr st_value; /* Symbol value */
+  Elf64_Xword st_size; /* Size of object (e.g., common) */
+} Elf64_Sym;
+
+#define PT_LOAD		1		/* Loadable program segment */
+#define	ELFMAG		"\177ELF"
+#define	SELFMAG		4
+#define EI_MAG0		0		/* File identification byte 0 index */
+#define SHF_ALLOC 0x2
+
+#define ELF64_R_SYM(i)((i) >> 32)
+#define ELF64_R_TYPE(i)((i) & 0xffffffffL)
+#define ELF64_R_INFO(s, t)(((s) << 32) + ((t) & 0xffffffffL))
+
+typedef struct
+{
+	uint32_t flags;
+	uint32_t mem_lower;
+	uint32_t mem_upper;
+	uint32_t boot_device;
+	uint32_t cmdline;
+	uint32_t mods_count;
+	uint32_t mods_addr;
+	uint32_t num;
+	uint32_t size;
+	uint32_t addr;
+	uint32_t shndx;
+	uint32_t mmap_length;
+	uint32_t mmap_addr;
+	uint32_t drives_length;
+	uint32_t drives_addr;
+	uint32_t config_table;
+	uint32_t boot_loader_name;
+	uint32_t apm_table;
+	uint32_t vbe_control_info;
+	uint32_t vbe_mode_info;
+	uint32_t vbe_mode;
+	uint32_t vbe_interface_seg;
+	uint32_t vbe_interface_off;
+	uint32_t vbe_interface_len;
+}GRUBMultiboot ;
+
+typedef struct{
+  /* the memory used goes from bytes ’mod_start’ to ’mod_end-1’ inclusive */
+  uint32_t mod_start;
+  uint32_t mod_end;
+
+  /* Module command line */
+  uint32_t cmdline;
+
+  /* padding to take it to 16 bytes (must be zero) */
+  uint32_t pad;
+}multiboot_mod_list;
+ 
+/* Hardware text mode color constants. */
+enum vga_color {
+	VGA_COLOR_BLACK = 0,
+	VGA_COLOR_BLUE = 1,
+	VGA_COLOR_GREEN = 2,
+	VGA_COLOR_CYAN = 3,
+	VGA_COLOR_RED = 4,
+	VGA_COLOR_MAGENTA = 5,
+	VGA_COLOR_BROWN = 6,
+	VGA_COLOR_LIGHT_GREY = 7,
+	VGA_COLOR_DARK_GREY = 8,
+	VGA_COLOR_LIGHT_BLUE = 9,
+	VGA_COLOR_LIGHT_GREEN = 10,
+	VGA_COLOR_LIGHT_CYAN = 11,
+	VGA_COLOR_LIGHT_RED = 12,
+	VGA_COLOR_LIGHT_MAGENTA = 13,
+	VGA_COLOR_LIGHT_BROWN = 14,
+	VGA_COLOR_WHITE = 15,
+};
+ 
+static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg) 
+{
+	return fg | bg << 4;
+}
+ 
+static inline uint16_t vga_entry(unsigned char uc, uint8_t color) 
+{
+	return (uint16_t) uc | (uint16_t) color << 8;
+}
+ 
+size_t strlen(const char* str) 
+{
+	size_t len = 0;
+	while (str[len])
+		len++;
+	return len;
+}
+ 
+static const size_t VGA_WIDTH = 80;
+static const size_t VGA_HEIGHT = 25;
+ 
+size_t terminal_row;
+size_t terminal_column;
+uint8_t terminal_color;
+uint16_t* terminal_buffer;
+ 
+void terminal_initialize(void) 
+{
+	terminal_row = 0;
+	terminal_column = 0;
+	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+	terminal_buffer = (uint16_t*) 0xB8000;
+	for (size_t y = 0; y < VGA_HEIGHT; y++) {
+		for (size_t x = 0; x < VGA_WIDTH; x++) {
+			const size_t index = y * VGA_WIDTH + x;
+			terminal_buffer[index] = vga_entry(' ', terminal_color);
+		}
+	}
+}
+ 
+void terminal_setcolor(uint8_t color) 
+{
+	terminal_color = color;
+}
+ 
+void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) 
+{
+	const size_t index = y * VGA_WIDTH + x;
+	terminal_buffer[index] = vga_entry(c, color);
+}
+ 
+void terminal_putchar(char c) 
+{
+    if(c=='\n'){
+        terminal_row++;
+        terminal_column = 0;
+        return;
+    }
+	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+	if (++terminal_column == VGA_WIDTH) {
+		terminal_column = 0;
+		if (++terminal_row == VGA_HEIGHT)
+			terminal_row = 0;
+	}
+}
+ 
+void terminal_write(const char* data, size_t size) 
+{
+	for (size_t i = 0; i < size; i++)
+		terminal_putchar(data[i]);
+}
+ 
+void terminal_writestring(const char* data) 
+{
+	terminal_write(data, strlen(data));
+}
+
+void nolongmodemessager()
+{
+    terminal_writestring("No long mode available\n");
+}
+
+char *convert(unsigned int num, int base) { 
+	static char Representation[]= "0123456789ABCDEF";
+	static char buffer[50]; 
+	char *ptr; 
+
+    for(int i = 0 ; i < 50 ; i++){
+        buffer[i] = 0x00;
+    }
+
+	if(num==0){
+		ptr = &buffer[0];
+		buffer[0] = '0';
+		return ptr;
+	}
+	
+	ptr = &buffer[49]; 
+	*--ptr = '\0'; 
+	*--ptr = '\0'; 
+	*--ptr = '\0'; 
+	
+	do 
+	{ 
+		*--ptr = Representation[num%base]; 
+		num /= base; 
+	}while(num != 0);
+	
+	return(ptr); 
+}
+
+typedef __builtin_va_list va_list;
+
+#define va_start(a,b)  __builtin_va_start(a,b)
+#define va_end(a)      __builtin_va_end(a)
+#define va_arg(a,b)    __builtin_va_arg(a,b)
+#define __va_copy(d,s) __builtin_va_copy((d),(s))
+
+void printf(char* format,...){
+    va_list arg; 
+	va_start(arg, format);
+    int length = 0;
+    while(1){
+        char deze = format[length];
+        if(deze=='\0'){
+            break;
+        }else if(deze=='%'){
+            length++;
+            deze = format[length];
+            if(deze=='c'){
+                char i = va_arg(arg,int);
+                terminal_putchar(i);
+            }else if(deze=='%'){
+                terminal_putchar('%');
+            }else if(deze=='s'){
+                char *s = va_arg(arg,char *);
+                terminal_writestring(s);
+            }else if(deze=='x'){
+                int t = va_arg(arg,unsigned int);
+                terminal_putchar('0');
+                terminal_putchar('x');
+                char *convertednumber = convert(t,16);
+                terminal_writestring(convertednumber);
+            }else if(deze=='d'){
+                int t = va_arg(arg,unsigned int);
+                char *convertednumber = convert(t,10);
+                terminal_writestring(convertednumber);
+            }else if(deze=='o'){
+                int t = va_arg(arg,unsigned int);
+                char *convertednumber = convert(t,8);
+                terminal_writestring(convertednumber);
+            }
+            length++;
+        }else{
+            terminal_putchar(deze);
+            length++;
+        }
+    }
+    va_end(arg);
+}
+
+uint64_t epoint;
+ 
+void kernel_main(GRUBMultiboot *grub, uint32_t magic) 
+{
+	/* Initialize terminal interface */
+	terminal_initialize();
+ 
+	/* Newline support is left as an exercise. */
+	terminal_writestring("Bootloader\n");
+    if(magic!=0x2BADB002){
+        terminal_writestring("Invalid header\n");
+        return;
+    }
+    if(grub->mods_count!=1){
+        terminal_writestring("mods_count!=1\n");
+        return;
+    }
+    multiboot_mod_list* modlist = (multiboot_mod_list*) grub->mods_addr;
+    void* dat = (void*) modlist->mod_start;
+    
+    Elf64_Ehdr* header = (Elf64_Ehdr*) dat;
+    epoint = header->e_entry;
+
+    printf("e_shnum: %d e_shentsize: %d \n",header->e_shnum,header->e_shentsize);
+    for(Elf64_Half i = 0 ; i < header->e_shnum ; i++){
+        Elf64_Shdr* shr = (Elf64_Shdr*) (header->e_shoff + dat + (header->e_shentsize*i));
+        if(shr->sh_flagsA&2){
+            printf("%d: flags: %x address: %x offset: %x size: %x \n",i,shr->sh_flagsA,shr->sh_addrA,shr->sh_offsetA,shr->sh_sizeA);
+            for(uint32_t z = 0 ; z < shr->sh_sizeA; z++){
+                ((uint8_t*)(shr->sh_addrA))[z] = ((uint8_t*)(shr->sh_offsetA + dat))[z];
+            }
+        }
+    }
+}
