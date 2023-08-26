8e372bc0da7ca3ce92716b41f5fb6180a51c5c48
Sander de Regt <alexandrosderegtvisvliet@gmail.com>
2023-08-26 10:02:42 +0200

    fasm fixes

--- kernel/drivers/idt.c
+++ kernel/drivers/idt.c
@@ -235,7 +235,7 @@ void isr2handler(stack_registers *ix){
 #endif 
     // k_printf("isr2: yield eax=%x ebx=%x ecx=%x edx=%x \n",ix->rax,ix->rbx,ix->rcx,ix->rdx);
     if(ix->rax==0){
-        k_printf("isr2:request read\n");
+        // k_printf("isr2:request read\n");
         File *fl = (File*) &(getCurrentTaskInfo()->files[ix->rbx]);
         memcpy((void*)ix->rdx,(void*)(fl->buffer + fl->pointer),ix->rcx);
         fl->pointer = fl->pointer + ix->rcx;
@@ -247,7 +247,7 @@ void isr2handler(stack_registers *ix){
                 k_printf("%c",z[i]);
             }
         }else{
-            k_printf("isr2:request write\n");
+            // k_printf("isr2:request write\n");
             upointer_t oldsize = getFileSize(fl->filename);
             upointer_t newsize = oldsize + ix->rdx;
             void *tmpbuf = requestPage();
@@ -265,7 +265,7 @@ void isr2handler(stack_registers *ix){
         // k_printf("isr2:request open rsi=%x \n",ix->rsi);for(;;);
         // fileopen option!
         char* path = (char*) ix->rdx;
-        k_printf("isr2:request to open %s \n",path);
+        // k_printf("isr2:request to open %s \n",path);
         upointer_t filesize = 0;
         void* buffer;
         if(ix->rsi==0){
@@ -295,13 +295,13 @@ void isr2handler(stack_registers *ix){
         // k_printf("Opening file with ID %d \n",sov);
         ix->rax = sov;
     }else if(ix->rax==3){
-        k_printf("isr2:request close\n");
+        // k_printf("isr2:request close\n");
         File *fl = (File*) &(getCurrentTaskInfo()->files[ix->rdi]);
         fl->available = 0;
         ix->rax = 0;
     }else if(ix->rax==8){
         // "D" (stream) , "d" (origin) , "S" (offset)
-        k_printf("isr2:request seek fileid(rbx):%d , args(rcx):%d , method(rdx):%d \n",ix->rbx,ix->rcx,ix->rdx);
+        // k_printf("isr2:request seek fileid(rbx):%d , args(rcx):%d , method(rdx):%d \n",ix->rbx,ix->rcx,ix->rdx);
         // seek option!
         File *fl = (File*) &(getCurrentTaskInfo()->files[ix->rbx]);
         if(ix->rcx==2){
@@ -312,25 +312,25 @@ void isr2handler(stack_registers *ix){
         ix->rax = fl->pointer;
         // ix->rax = 0;
     }else if(ix->rax==12){
-        k_printf("isr2:request space\n");
+        // k_printf("isr2:request space\n");
         // we always accept extending memory whatoever
         ix->rax = ix->rdi;
     }else if(ix->rax==60){
-        k_printf("isr2: end\n");
+        // k_printf("isr2: end\n");
         Task* ts = (Task*) (getTasks() + (sizeof(Task)*getPid()));
         ts->task_running = 0;
         ix->rip = (upointer_t)tty_inner_loop;
     }else if(ix->rax==96){
-        k_printf("isr2:request time\n");
+        // k_printf("isr2:request time\n");
         timeval* tv = (timeval*) ix->rdi;
         tv->tv_sec = 10000;
         tv->tv_usec = 10100;
         ix->rax = 0;
     }else if(ix->rax==400){
-        k_printf("isr2:request page\n");
+        // k_printf("isr2:request page\n");
         ix->rax = (upointer_t) requestPage();
     }else if(ix->rax==401){
-        k_printf("isr2:request argument\n");
+        // k_printf("isr2:request argument\n");
         if(ix->rcx>0&&ix->rcx<10){
             char* us = (char*) getCurrentTaskInfo()->arguments[ix->rcx-1];
             ((uint32_t*)ix->rdi)[0] = (uint32_t)((upointer_t)us);
@@ -350,27 +350,27 @@ void isr2handler(stack_registers *ix){
         u[0] = getch(1);
     }else if(ix->rax==405){
         uint8_t *name = (uint8_t*) ix->rbx;
-        k_printf("isr2:request ip from name\n");
+        // k_printf("isr2:request ip from name\n");
         ix->rax = (upointer_t)getIPFromName(name);
     }else if(ix->rax==406){
-        k_printf("isr2:request mac from ip\n");
+        // k_printf("isr2:request mac from ip\n");
         uint8_t *name = (uint8_t*) ix->rbx;
         ix->rax = (upointer_t)getMACFromIp(name);
     }else if(ix->rax==407){
-        k_printf("isr2:request tcp session\n");
+        // k_printf("isr2:request tcp session\n");
         create_tcp_session(getOurIpAsLong(),((unsigned long*)ix->rbx)[0],ix->rcx,ix->rcx,ix->rdx);
     }else if(ix->rax==408){
-        k_printf("isr2:request sleep\n");
+        // k_printf("isr2:request sleep\n");
         sleep(ix->rbx);
     }else if(ix->rax==409){
-        k_printf("isr2:release page\n");
+        // k_printf("isr2:release page\n");
         freePage((void*)ix->rbx);
     }else if(ix->rax==410){
-        k_printf("isr2:request env variable [%s] \n",(char*) ix->rdx);
+        // k_printf("isr2:request env variable [%s] \n",(char*) ix->rdx);
         char* p = "A:INCLUDE/";
         ix->rax = (upointer_t)p;
     }else if(ix->rax==411){
-        k_printf("isr2:request tell \n");
+        // k_printf("isr2:request tell \n");
         File *fl = (File*) &(getCurrentTaskInfo()->files[ix->rbx]);
         ix->rax = fl->pointer;
     }else{
