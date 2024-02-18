ede080c556aaa82687b49b490a771ecaa823a657
josip-krnic <krnic.josip.jk@gmail.com>
2024-02-18 20:17:06 +0100

    Added some functions to math library.

--- programsapi/include/math.h
+++ programsapi/include/math.h
@@ -1,19 +1,16 @@
 #ifndef MATH_H
 #define MATH_H
 
-long pow(int base, unsigned exp); // Returns x raised to the power of y.
+#define PI 3.14159265359
+#define E 2.71828182846
+
+float power(float base, int exp); // Returns x raised to the power of y.
 double sqrt(double x); // Returns square root of x.
 unsigned fact(unsigned x); // Returns factoriel of x.
+double dmod(double x, double y); // Modulo for double.
 
-// Trigonometry functions are calculated via Taylor's series
-
-double sin(double x); // Returns the sine of a radian angle x.
-double cos(double x); // Returns the cosine of a radian angle x.
+float sin(double deg); // Returns the sine of a radian angle x.
+float cos(double deg); // Returns the cosine of a radian angle x.
 double tan(double x); // Returns the tangent of a radian angle x.
-double cotan(double x); // Returns the cosine of a radian angle x.
-
-double asin(double x); // Returns the arc sine of x in radians.
-double acos(double x); // Returns the arc cosine of x in radians.
-double atan(double x); // Returns the arc tangent of x in radians.
 
 #endif
--- programsapi/math.c
+++ programsapi/math.c
@@ -1,85 +1,70 @@
 #include "math.h"
 
-#define TRIG_PREC 30 // calculates to what precision calculate trigonometry functions
+#define MINDIFF 2.25e-308 // use for convergence check
+#define TERMS 7
 
-long pow(int base, unsigned exp)
-{
-    long res = base;
-    for (int i = 1; i < ++exp; i++)
-        base *= base;
 
-    return res;
+float power(float base, int exp) {
+    if(exp < 0) {
+        if(base == 0)
+            return -0; // Error! 0^0
+        return 1 / (base * power(base, (-exp) - 1));
+    }
+    if(exp == 0)
+        return 1;
+    if(exp == 1)
+        return base;
+    return base * power(base, exp - 1);
 }
 
 double sqrt(double x)
 {
+    double root=x/3, last, diff=1;
+    if (x <= 0) return 0;
+    do {
+        last = root;
+        root = (root + x / root) / 2;
+        diff = root - last;
+    } while (diff > MINDIFF || diff < -MINDIFF);
+    return root;
 }
 
 unsigned fact(unsigned x)
 {
-    int res = 1;
-
-    for (int i = 1; i < x + 1; i++)
-        res *= i;
-
-    return res;
+    return x <= 0 ? 1 : x * fact(x-1);
 }
 
-double sin(double x)
-{
-    double numerator = 0;
-    double denominator = 0;
-    double result = 0;
-
-    // brojnik = [(-1)^k] * x ^ (2k+1)
-    // nazivnik = (2k+1)!
-    for (int i = 0; i < TRIG_PREC; i += 2)
-    {
-        numerator += pow(x, 2*i + 1);
-        denominator += fact(2*i + 1);
-        result += numerator / denominator;
-        numerator = 0;
-        denominator = 0;
-    }
-
-    for (int i = 1; i < TRIG_PREC; i += 2)
-    {
-        numerator -= pow(x, 2*i + 1);
-        denominator += fact(2*i + 1);
-        result += numerator / denominator;
-        numerator = 0;
-        denominator = 0;
-    }
-
-    return result;
+double dmod(double x, double y) {
+    return x - (int)(x/y) * y;
 }
 
-double cos(double x)
-{
+float sin(double deg) {
+    deg = dmod(deg, 360.0f);
+    float rad = deg * PI / 180;
+    float sin = 0;
 
+    int i;
+    for(i = 0; i < TERMS; i++) 
+    { 
+        sin += power(-1, i) * power(rad, 2 * i + 1) / fact(2 * i + 1);
+    }
+    return sin;
+}
+
+float cos(double deg) {
+    deg = dmod(deg, 360.0f); 
+    float rad = deg * PI / 180;
+    float cos = 0;
+
+    int i;
+    for(i = 0; i < TERMS; i++) 
+    { 
+        cos += pow(-1, i) * pow(rad, 2 * i) / fact(2 * i);
+    }
+    return cos;
 }
 
 double tan(double x)
 {
     return sin(x)/cos(x);
-}
-
-double cotan(double x) 
-{
-
-}
-
-double asin(double x)
-{
-
-}
-
-double acos(double x)
-{
-
-}
-
-double atan(double x)
-{
-
 }
